1f5a18d4096120718e6592a39868ccfdaffdb312
Ben Hammond <benjamin.hammond@gmail.com>
2023-09-01 18:53:22 -0600

    check in

---
+++ README.md
@@ -0,0 +1,11 @@
+# Rust Big Data Playground
+
+Just a place for me to mess around while learning Rust; potentially for usage in the https://healthequitytracker.org
+
+- If you want to see the kinds of equity / health data I work with daily, check out the [HET repo on GitHub](https://github.com/SatcherInstitute/health-equity-tracker)
+- To see my other projects, check out out my developer portfolio at https://benhammond.tech
+- To see my professional musician site, check out https://benhammondmusic.com
+
+
+
+
---
+++ src/main.rs
@@ -0,0 +1,47 @@
+extern crate csv;
+#[macro_use]
+extern crate serde_derive;
+// use chrono::NaiveDate;
+
+use std::error::Error;
+use std::fs::File;
+use std::process;
+
+// By default, struct field names are deserialized based on the position of
+// a corresponding field in the CSV data's header record.
+#[derive(Debug, Deserialize)]
+struct Record {
+    state_name: String,
+    race: String,
+    // column_3: String,
+    // column_4: u32,
+    // column_5: String,
+}
+
+fn example() -> Result<(), Box<dyn Error>> {
+    let file = File::open("_restricted_small_data.csv")?;
+    let mut count_deaths = 0;
+
+    let mut rdr = csv::Reader::from_reader(file);
+    for result in rdr.deserialize() {
+        // Notice that we need to provide a type hint for automatic
+        // deserialization.
+        let record: Record = result?;
+        // println!("{:?}", record);
+
+        if record.state_name == "Colorado" && record.race == "White" {
+            count_deaths += 1;
+        }
+    }
+
+    println!("Number of White Deaths: {}", count_deaths);
+
+    Ok(())
+}
+
+fn main() {
+    if let Err(err) = example() {
+        println!("error running example: {}", err);
+        process::exit(1);
+    }
+}
