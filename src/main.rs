89d91a4565a17100c31f290b1323390cce6639ff
Ben Hammond <benjamin.hammond@gmail.com>
2023-09-01 20:25:26 -0600

    polars

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,14 @@
-extern crate csv;
-#[macro_use]
-extern crate serde_derive;
-// use chrono::NaiveDate;
+use polars_core::prelude::*;
+use polars_io::prelude::*;
+// use std::fs::File;
 
-use std::error::Error;
-use std::fs::File;
-use std::process;
-
-// By default, struct field names are deserialized based on the position of
-// a corresponding field in the CSV data's header record.
-#[derive(Debug, Deserialize)]
-struct Record {
-    state_name: String,
-    race: String,
-    // column_3: String,
-    // column_4: u32,
-    // column_5: String,
-}
-
-fn example() -> Result<(), Box<dyn Error>> {
-    let file = File::open("small_data.csv")?;
-    let mut count_deaths = 0;
-
-    let mut rdr = csv::Reader::from_reader(file);
-    for result in rdr.deserialize() {
-        // Notice that we need to provide a type hint for automatic
-        // deserialization.
-        let record: Record = result?;
-        // println!("{:?}", record);
-
-        if record.state_name == "Colorado" && record.race == "White" {
-            count_deaths += 1;
-        }
-    }
-
-    println!("#: {}", count_deaths);
-
-    Ok(())
+fn example() -> PolarsResult<DataFrame> {
+    return CsvReader::from_path("small_data.csv")?
+        .has_header(true)
+        .finish();
 }
 
 fn main() {
-    if let Err(err) = example() {
-        println!("error running example: {}", err);
-        process::exit(1);
-    }
+    let df = example().expect("oops");
+    print!("{}", df.to_string());
 }
