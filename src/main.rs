6bef21f3a9abfeaae3bd4c519c8f8cbd9ac0884d
Ben Hammond <benjamin.hammond@gmail.com>
2023-09-02 15:22:26 -0600

    polars aggs working

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
+use polars::{
+    lazy::dsl::col,
+    prelude::{LazyCsvReader, LazyFileListReader},
+};
 use polars_core::prelude::*;
-use polars_io::prelude::*;
-// use std::fs::File;
+use polars_io::{prelude::CsvWriter, SerWriter};
 
-fn example() -> PolarsResult<DataFrame> {
-    return CsvReader::from_path("small_data.csv")?
+fn load_csv_and_agg() -> Result<DataFrame, PolarsError> {
+    LazyCsvReader::new("small_data.csv")
         .has_header(true)
-        .finish();
+        .finish()?
+        .groupby(vec![col("state_name"), col("race")])
+        .agg(vec![col("case_count").sum()])
+        .sort(
+            "state_name",
+            SortOptions {
+                descending: false,
+                nulls_last: true,
+                multithreaded: false,
+                maintain_order: false,
+            },
+        )
+        .collect()
 }
 
 fn main() {
-    let df = example().expect("oops");
-    print!("{}", df.to_string());
+    let mut df = load_csv_and_agg().expect("oops");
+    println!("{:?}", df);
+
+    let mut file = std::fs::File::create("results.csv").unwrap();
+    CsvWriter::new(&mut file).finish(&mut df).unwrap();
 }
