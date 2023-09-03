fc9b41200c18bed0084eb2b14949e966fafd0e3f
Ben Hammond <benjamin.hammond@gmail.com>
2023-09-03 10:18:02 -0600

    sums every row to derive case count

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,43 @@
 use polars::{
-    lazy::dsl::col,
+    lazy::dsl::{col, lit, when},
     prelude::{LazyCsvReader, LazyFileListReader},
 };
 use polars_core::prelude::*;
 use polars_io::{prelude::CsvWriter, SerWriter};
 
 fn load_csv_and_agg() -> Result<DataFrame, PolarsError> {
+    let groupby_cols = vec![
+        col("date"),
+        col("state_name"),
+        col("race"),
+        col("age"),
+        col("sex"),
+    ];
+
     LazyCsvReader::new("small_data.csv")
         .has_header(true)
         .finish()?
-        .groupby(vec![col("state_name"), col("race")])
-        .agg(vec![col("case_count").sum()])
-        .sort(
-            "state_name",
-            SortOptions {
-                descending: false,
-                nulls_last: true,
-                multithreaded: false,
-                maintain_order: false,
-            },
+        .with_column(
+            when(col("date").is_not_null())
+                .then(lit(1))
+                .otherwise(lit(0))
+                .alias("case_yn"),
         )
+        .groupby(groupby_cols)
+        .agg(vec![
+            col("case_yn").sum(),
+            col("hosp_yn").sum(),
+            col("death_yn").sum(),
+        ])
         .collect()
 }
 
 fn main() {
     let mut df = load_csv_and_agg().expect("oops");
+
+    let sort_cols = ["date", "state_name", "race", "age", "sex"];
+
+    df = df.sort(sort_cols, false, false).expect("oops");
     println!("{:?}", df);
 
     let mut file = std::fs::File::create("results.csv").unwrap();
