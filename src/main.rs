683b72233b37641c1932ab6b36bf3568b300473c
Ben Hammond <benjamin.hammond@gmail.com>
2023-09-04 10:11:17 -0600

    combines multiple csvs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use polars::{
     lazy::dsl::{col, lit, when},
-    prelude::{LazyCsvReader, LazyFileListReader},
+    prelude::{LazyCsvReader, LazyFileListReader, LazyFrame, UnionArgs},
 };
 use polars_core::prelude::*;
 use polars_io::{prelude::CsvWriter, SerWriter};
@@ -9,14 +9,44 @@ use polars_io::{prelude::CsvWriter, SerWriter};
 
 // Same fake data as currently used by cdc_restricted_local
 const TEST_FILENAME_PART_1: &str = "COVID_Cases_Restricted_Detailed_04302021_Part_1.csv";
-// const TEST_FILENAME_PART_2: &str = "COVID_Cases_Restricted_Detailed_04302021_Part_2.csv";
+const TEST_FILENAME_PART_2: &str = "COVID_Cases_Restricted_Detailed_04302021_Part_2.csv";
 
-fn load_csv_and_agg() -> Result<DataFrame, PolarsError> {
-    let test_filepath: String = format!("tests/fake_source_data/{TEST_FILENAME_PART_1}");
+fn read_csvs_as_lazyframe() -> Result<LazyFrame, PolarsError> {
+    let test_filepath1: String = format!("tests/fake_source_data/{TEST_FILENAME_PART_1}");
+    let test_filepath2: String = format!("tests/fake_source_data/{TEST_FILENAME_PART_2}");
 
+    // List of CSV file paths
+    let file_paths = vec![test_filepath1, test_filepath2];
+
+    // Create an empty Vec to store the LazyFrames
+    let mut lazy_frames: Vec<LazyFrame> = Vec::new();
+
+    // Read each CSV file into a LazyFrame and store them in the Vec
+    for file_path in file_paths.iter() {
+        let lf = LazyCsvReader::new(file_path)
+            .has_header(true)
+            .with_quote_char(None)
+            .finish()?;
+        lazy_frames.push(lf);
+    }
+
+    // Concatenate the LazyFrames into a single LazyFrame
+    let combined_lazy_frame = polars::prelude::concat(
+        lazy_frames,
+        UnionArgs {
+            parallel: false,
+            rechunk: false,
+            to_supertypes: false,
+        },
+    )?;
+
+    Ok(combined_lazy_frame)
+}
+
+fn process_lazyframe_into_aggregated_df(lf: LazyFrame) -> Result<DataFrame, PolarsError> {
     let known_sex_groups = vec!["Male", "Female", "Other"];
     let know_sex_series = Series::new("KNOWN_SEX_GROUP", known_sex_groups);
-    let filter_known_sex_groups = col("sex").is_in(lit(know_sex_series));
+    let is_known_sex_group = col("sex").is_in(lit(know_sex_series));
 
     let groupby_cols = vec![
         col("time_period"),
@@ -27,10 +57,7 @@ fn load_csv_and_agg() -> Result<DataFrame, PolarsError> {
         col("sex"),
     ];
 
-    LazyCsvReader::new(test_filepath)
-        .has_header(true)
-        .with_quote_char(None)
-        .finish()?
+    let df = lf
         .rename(["res_state"], ["state_postal"])
         // "time_period" as cdc col with only YYYY-MM
         .with_column((col("cdc_case_earliest_dt").str().str_slice(0, Some(7))).alias("time_period"))
@@ -53,7 +80,7 @@ fn load_csv_and_agg() -> Result<DataFrame, PolarsError> {
         )
         // only keep Male/Female/Other/Unknown options for sex
         .with_column(
-            when(filter_known_sex_groups)
+            when(is_known_sex_group)
                 .then(col("sex"))
                 .otherwise(lit("Unknown")),
         )
@@ -67,27 +94,47 @@ fn load_csv_and_agg() -> Result<DataFrame, PolarsError> {
             col("death_n").sum(),
             col("death_unknown").sum(),
         ])
-        .collect()
+        .collect();
+
+    df
 }
 
 fn main() {
-    let mut df = load_csv_and_agg().expect("Problem with load_csv_and_agg()");
+    match read_csvs_as_lazyframe() {
+        Ok(lazy_frame) => {
+            println!("LazyFrame loaded successfully!");
+            match process_lazyframe_into_aggregated_df(lazy_frame) {
+                Ok(mut df) => {
+                    println!("LazyFrame Processed and Aggregated successfully!");
+                    println!("{:?}", df);
 
-    let sort_cols = [
-        "time_period",
-        "state_postal",
-        // "race",
-        // "age_group",
-        "sex",
-    ];
+                    let sort_cols = [
+                        "time_period",
+                        "state_postal",
+                        // "race",
+                        // "age_group",
+                        "sex",
+                    ];
 
-    df = df
-        .sort(sort_cols, false, false)
-        .expect("Problem sorting df");
-    println!("{:?}", df);
+                    df = df
+                        .sort(sort_cols, false, false)
+                        .expect("Problem sorting df");
+                    println!("{:?}", df);
 
-    let mut file = std::fs::File::create("results.csv").unwrap();
-    CsvWriter::new(&mut file).finish(&mut df).unwrap();
+                    let mut file = std::fs::File::create("results.csv").unwrap();
+                    CsvWriter::new(&mut file).finish(&mut df).unwrap();
+                }
+                Err(err) => {
+                    // Handle the error if there was any
+                    eprintln!("Error: {:?}", err);
+                }
+            }
+        }
+        Err(err) => {
+            // Handle the error if there was any
+            eprintln!("Error: {:?}", err);
+        }
+    }
 }
 
 /*
